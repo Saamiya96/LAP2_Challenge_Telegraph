a7b5f272b3c41c93e56326f7d9eaa4b28e583b57
saamiya96 <saamiyayousuf@hotmail.co.uk>
2022-07-12 21:25:11 +0100

    created basic server for backend

---
+++ server/api/routes/posts.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const posts = require('../controllers/posts')
+
+// router.get('/')
+// router.get('/:id)
+// router.post('/')
+
+module.exports = router;
